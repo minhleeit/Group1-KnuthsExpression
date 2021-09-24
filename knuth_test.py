6849094b9b7ce90af48a7738d373af1044714b10
scadamsUAB <scadams@uab.edu>
2021-09-24 15:07:33 -0500

    updated some code based on group meeting

--- knuth_test.py
+++ knuth_test.py
@@ -1,4 +1,8 @@
 import math
+from decimal import Decimal
+
+seen_states = {}
+state_list =["3"]
 
 def perform_action(current_value, action):
     if current_value == 0 and action != "3":
@@ -9,19 +13,18 @@ def perform_action(current_value, action):
         return 3
 
     elif action == "sqrt":
-        return math.sqrt(current_value)
+        return Decimal(math.sqrt(current_value))
     
     elif action == "fact":
-        return math.factorial(current_value)
+        return Decimal(math.factorial(current_value))
     
     elif action == "floor": 
-        return math.floor(current_value)
+        return math.floor(Decimal(current_value))
 
     else:
         print("Invalid action found: " + str(action))
         exit()
     
-
 def evaulate_expression(evalute_expression):
     index = 0
     current_value = 0
@@ -30,39 +33,83 @@ def evaulate_expression(evalute_expression):
     for index in range(0,len(actions)):
         current_value = perform_action(current_value,actions[index])
     print("RESULT IS " + str(current_value))
-
-evaulate_expression("3 fact sqrt floor")
-
-evaulate_expression("3 floor")
-
-evaulate_expression("3 fact fact")
-
-evaulate_expression("3 fact fact sqrt")
-
-evaulate_expression("3 fact fact sqrt sqrt")
-
-evaulate_expression("3 fact fact sqrt sqrt floor")
-
-evaulate_expression("3 fact fact sqrt sqrt floor fact")
-
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt")
-
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact")
-
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt")
+    return current_value
 
 
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt")
+def check_if_goal_reached(state, target): 
+    result = evaulate_expression(state)
+    if ( target == result):
+        print("TARGET FOUND")
+        return True
+    elif target > 2 and result < 2:
+        pass
+    else:
+        if str(result) in seen_states :
+            print("STATE SEEN", result)
 
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact")
+        else:
+            print("ADDING RESULT", result)
+            seen_states[str(result)] = state
+            add_successors(state, result)    
+            print()
+            print("CURRENT LIST: ", state_list)
+            print()
+            return False
 
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt floor fact")
+def add_successors(state, result ):
+    print("TYPE OF RESULT: ", type(result))
+    if not isinstance(result, int):
+        state_list.append(str(state) + " floor")
+        state_list.append(str(state) + " sqrt")
 
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt floor fact sqrt")
-evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt floor fact sqrt floor sqrt")
+    else: 
+        state_list.append(str(state) + " sqrt")
+        state_list.append(str(state) + " fact")
+        state_list.append(str(state) + " floor")
+
+def advance(target):
+ 
+    while len(state_list) >0:
+        next_item = state_list.pop()
+        print("NEXT ITEM: ", next_item)
+        if check_if_goal_reached(next_item,target):
+            break
+        
+
+target = 5
+advance(target)
+# evaulate_expression("3 fact sqrt floor")
+
+# evaulate_expression("3 floor")
+
+# evaulate_expression("3 fact fact")
+
+# evaulate_expression("3 fact fact sqrt")
+
+# evaulate_expression("3 fact fact sqrt sqrt")
+        
+# evaulate_expression("3 fact fact sqrt sqrt floor")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt")
+
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt floor fact")
+
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt floor fact sqrt")
+# evaulate_expression("3 fact fact sqrt sqrt floor fact sqrt floor fact sqrt sqrt floor fact sqrt sqrt sqrt sqrt sqrt sqrt floor fact sqrt floor sqrt")
